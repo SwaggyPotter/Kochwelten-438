274c31ce83f9804551b60047cf268fbaa98f7161
Zwogli <Mathiaskohler@web.de>
2023-01-31 13:54:28 +0100

    clean script

--- script.js
+++ script.js
@@ -1,58 +1,4 @@
-// <<<<<<<<<< Rezept JSON{}, Array [] >>>>>>>>>>>>>
-let recipes = [
-{
-    // Head
-    "name" : "Soljanka nach Mama Art",
-    "img" : `img/soljanka.jpg`,
-    // cards
-    "time" : "1 Stunde 15 Minuten",
-    "difficulty" : "normal",
-    "calender" : "28.01.2023",
-    // "calories" : "674 kcal",
-// 
-// <<<<<<<<<<<< other card
-//     
-    // table quantity for 1 person
-    "portion" : 4,
-    "quantity" : ['50', '50', '50', '50', '1.25','1','37.5','1.25','0.25','0.25','0.25','0.75','','',''],
-    "unit" : ['g','g','g','g','Anzahl','Anzahl','g','Anzahl','EL','Anzahl','TL','Zehen','',''],
-    "unit" : ['g','g','g','g','Anzahl','Anzahl','g','Anzahl','EL','Anzahl','TL','Zehen','','',''],
-    "ingredient" : ['Salami','Jagdwurst','Geflügelwurst','Schinkenspeck, gewürfelt','Zwiebel(n)',`Paprikaschote(n)`,'Tomatenmark','Gewürzgurke(n)','Sambal Olek','Lorbeerblatt','Senf','Knoblauch','Gurkensud','Brühe, fette','saure Sahne'],
-    "preparation" : `Den Speck anbraten, die gewürfelte Wurst dazugeben (noch nicht die Salami!) und alles schön anbraten. Dann die Salami dazugeben. Kurz weiterbraten, die Zwiebel mit in den Topf geben und weiterbraten, bis die Zwiebeln glasig sind. Falls es ansetzen sollte, ein wenig Wasser dazugeben.<br>
-                    Nun die gewürfelten Paprika, das Tomatenmark und den zerdrückten Knoblauch dazugeben, etwas anbraten. Dann die klein geschnittenen Gurken, etwas Gurkenwasser und sämtliche Gewürze dazugeben. Mit 1 bis 1 1/2 Liter Wasser aufgießen, die fette Brühe dazugeben und 20 Minuten leicht köcheln lassen.<br>
-                    <br>
-                    Idealerweise bereitet man die Soljanka am Vorabend zu. Am nächsten Tag nur noch aufwärmen.<br>
-                    Wichtig! Die Soljanka muss ein paar Stunden stehen, bevor man sie isst.<br>
-                    <br>
-                    Dazu reiche ich immer geröstetes Ciabattabrot und saure Sahne zum Einrühren.`,
-    // creator
-    "profilPic" : `img/profileMathiasKohler.jpg`,
-    "creator" : "Mathias Kohler",
-},
-{
-    "name" : "Soljanka nach Mama Art",
-    "img" : `img/soljanka.jpg`,
-    "time" : "1 Stunde 15 Minuten",
-    "difficulty" : "normal",
-    "calender" : "28.01.2023",
-    "portion" : 4,
-    "quantity" : ['50', '50', '50', '50', '1.25','1','37.5','1.25','0.25','0.25','0.25','0.75','','',''],
-    "unit" : ['g','g','g','g','Anzahl','Anzahl','g','Anzahl','EL','Anzahl','TL','Zehen','',''],
-    "unit" : ['g','g','g','g','Anzahl','Anzahl','g','Anzahl','EL','Anzahl','TL','Zehen','','',''],
-    "ingredient" : ['Salami','Jagdwurst','Geflügelwurst','Schinkenspeck, gewürfelt','Zwiebel(n)',`Paprikaschote(n)`,'Tomatenmark','Gewürzgurke(n)','Sambal Olek','Lorbeerblatt','Senf','Knoblauch','Gurkensud','Brühe, fette','saure Sahne'],
-    "preparation" : `Den Speck anbraten, die gewürfelte Wurst dazugeben (noch nicht die Salami!) und alles schön anbraten. Dann die Salami dazugeben. Kurz weiterbraten, die Zwiebel mit in den Topf geben und weiterbraten, bis die Zwiebeln glasig sind. Falls es ansetzen sollte, ein wenig Wasser dazugeben.<br>
-                    Nun die gewürfelten Paprika, das Tomatenmark und den zerdrückten Knoblauch dazugeben, etwas anbraten. Dann die klein geschnittenen Gurken, etwas Gurkenwasser und sämtliche Gewürze dazugeben. Mit 1 bis 1 1/2 Liter Wasser aufgießen, die fette Brühe dazugeben und 20 Minuten leicht köcheln lassen.<br>
-                    <br>
-                    Idealerweise bereitet man die Soljanka am Vorabend zu. Am nächsten Tag nur noch aufwärmen.<br>
-                    Wichtig! Die Soljanka muss ein paar Stunden stehen, bevor man sie isst.<br>
-                    <br>
-                    Dazu reiche ich immer geröstetes Ciabattabrot und saure Sahne zum Einrühren.`,
-    "profilPic" : `img/profileMathiasKohler.jpg`,
-    "creator" : "Mathias Kohler",
-},
-]
-
-// >>Load localStorage
+// >>Load localStorage for Rendering startPage
 // function render(){
 //     for( let i = 0 ; i < recipes.length, i++;){
 //         const recipe = recipes[i]; //with localStorage.setItem('recipeNumber',`${i}`) save all JSON´s
@@ -60,32 +6,12 @@ let recipes = [
 //     localStorage.setItem('recipeNumber',`${i}`);
 // }
 // >>open RecipeSite
-function addrecipe(i){//load with param
-    window.open(`recipe.html`,'_blank');
+function addrecipe(i){//load recipe with param
+    window.open(`recipe.html`,'_blank');//open new page
     localStorage.setItem('recipeNumber',`${i}`);//save 'key' , 'param local'
 }
-function renderTable(){
-    let i = localStorage.getItem('recipeNumber', '${i}');
-    let recipe = recipes[i];
-    document.getElementById("table-ingredient").innerHTML = '';// delete Table
-    for(a = 0 ; a < recipe["ingredient"].length ; a++){//search Array
-        let ingredient = recipe["ingredient"][a];
-        let unit = recipe["unit"][a];
-        let quantity = recipe["quantity"][a];
-        let currentlyPortion = document.getElementById('input-portion').value;//portion quantity
-        // create Table
-    document.getElementById("table-ingredient").innerHTML += /*html*/`
-    <tr>
-        <td class="td-number">${quantity * currentlyPortion}</td>
-        <td class="td-unit">${unit}</td>
-        <td class="td-ingredient">${ingredient}</td>
-    </tr>
-    `;
-}
-}
-// push section-recipe-head
-function renderRecipe(){
-    let i = localStorage.getItem('recipeNumber', '${i}');//load local(addrecipe(param))
+function renderRecipe(){//onload recipe.html
+    let i = localStorage.getItem('recipeNumber', '${i}');//load localStorage(addrecipe(param))
     let recipe = recipes[i];//load JSON
     
     document.getElementById(`recipe-headline`).innerHTML = `${recipe["name"]}`;//load JSON[Array]
@@ -101,4 +27,24 @@ function renderRecipe(){
     document.getElementById(`name-creator`).innerHTML = `${recipe["creator"]}`;
 
     renderTable();
+}
+function renderTable(){
+    let i = localStorage.getItem('recipeNumber', '${i}');
+    let table = document.getElementById("table-ingredient");
+    table.innerHTML = '';// delete Table
+    let currentlyPortion = document.getElementById('input-portion').value;//portion quantity    
+    let recipe = recipes[i];
+    for(a = 0 ; a < recipe["ingredient"].length ; a++){//search Array
+        let ingredient = recipe["ingredient"][a];
+        let unit = recipe["unit"][a];
+        let quantity = recipe["quantity"][a];
+        // create Table
+        table.innerHTML += /*html*/`
+    <tr>
+        <td class="td-number">${quantity * currentlyPortion}</td>
+        <td class="td-unit">${unit}</td>
+        <td class="td-ingredient">${ingredient}</td>
+    </tr>
+    `;
+}
 }
