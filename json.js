bce561be7c739b2b2d5b749547cce3d192df385e
Zwogli <Mathiaskohler@web.de>
2023-01-31 13:53:45 +0100

    outsrc JSON

---
+++ json.js
@@ -0,0 +1,53 @@
+// <<<<<<<<<< Rezept JSON{}, Array [] >>>>>>>>>>>>>
+let recipes = [
+    {
+        // Head
+        "name" : "Soljanka nach Mama Art",
+        "img" : `img/soljanka.jpg`,
+        // lables
+        "time" : "1 Stunde 15 Minuten",
+        "difficulty" : "normal",
+        "calender" : "28.01.2023",
+        // "calories" : "674 kcal",
+    // 
+    // <<<<<<<<<<<< other card
+    //     
+        // table quantity for 1 person
+        "portion" : 4,
+        "quantity" : ['50', '50', '50', '50', '1.25','1','37.5','1.25','0.25','0.25','0.25','0.75','','',''],
+        "unit" : ['g','g','g','g','Anzahl','Anzahl','g','Anzahl','EL','Anzahl','TL','Zehen','',''],
+        "unit" : ['g','g','g','g','Anzahl','Anzahl','g','Anzahl','EL','Anzahl','TL','Zehen','','',''],
+        "ingredient" : ['Salami','Jagdwurst','Geflügelwurst','Schinkenspeck, gewürfelt','Zwiebel(n)',`Paprikaschote(n)`,'Tomatenmark','Gewürzgurke(n)','Sambal Olek','Lorbeerblatt','Senf','Knoblauch','Gurkensud','Brühe, fette','saure Sahne'],
+        "preparation" : `Den Speck anbraten, die gewürfelte Wurst dazugeben (noch nicht die Salami!) und alles schön anbraten. Dann die Salami dazugeben. Kurz weiterbraten, die Zwiebel mit in den Topf geben und weiterbraten, bis die Zwiebeln glasig sind. Falls es ansetzen sollte, ein wenig Wasser dazugeben.<br>
+                        Nun die gewürfelten Paprika, das Tomatenmark und den zerdrückten Knoblauch dazugeben, etwas anbraten. Dann die klein geschnittenen Gurken, etwas Gurkenwasser und sämtliche Gewürze dazugeben. Mit 1 bis 1 1/2 Liter Wasser aufgießen, die fette Brühe dazugeben und 20 Minuten leicht köcheln lassen.<br>
+                        <br>
+                        Idealerweise bereitet man die Soljanka am Vorabend zu. Am nächsten Tag nur noch aufwärmen.<br>
+                        Wichtig! Die Soljanka muss ein paar Stunden stehen, bevor man sie isst.<br>
+                        <br>
+                        Dazu reiche ich immer geröstetes Ciabattabrot und saure Sahne zum Einrühren.`,
+        // creator
+        "profilPic" : `img/profileMathiasKohler.jpg`,
+        "creator" : "Mathias Kohler",
+    },
+    {
+        "name" : "Soljanka nach Mama Art",
+        "img" : `img/soljanka.jpg`,
+        "time" : "1 Stunde 15 Minuten",
+        "difficulty" : "normal",
+        "calender" : "28.01.2023",
+        "portion" : 4,
+        "quantity" : ['50', '50', '50', '50', '1.25','1','37.5','1.25','0.25','0.25','0.25','0.75','','',''],
+        "unit" : ['g','g','g','g','Anzahl','Anzahl','g','Anzahl','EL','Anzahl','TL','Zehen','',''],
+        "unit" : ['g','g','g','g','Anzahl','Anzahl','g','Anzahl','EL','Anzahl','TL','Zehen','','',''],
+        "ingredient" : ['Salami','Jagdwurst','Geflügelwurst','Schinkenspeck, gewürfelt','Zwiebel(n)',`Paprikaschote(n)`,'Tomatenmark','Gewürzgurke(n)','Sambal Olek','Lorbeerblatt','Senf','Knoblauch','Gurkensud','Brühe, fette','saure Sahne'],
+        "preparation" : `Den Speck anbraten, die gewürfelte Wurst dazugeben (noch nicht die Salami!) und alles schön anbraten. Dann die Salami dazugeben. Kurz weiterbraten, die Zwiebel mit in den Topf geben und weiterbraten, bis die Zwiebeln glasig sind. Falls es ansetzen sollte, ein wenig Wasser dazugeben.<br>
+                        Nun die gewürfelten Paprika, das Tomatenmark und den zerdrückten Knoblauch dazugeben, etwas anbraten. Dann die klein geschnittenen Gurken, etwas Gurkenwasser und sämtliche Gewürze dazugeben. Mit 1 bis 1 1/2 Liter Wasser aufgießen, die fette Brühe dazugeben und 20 Minuten leicht köcheln lassen.<br>
+                        <br>
+                        Idealerweise bereitet man die Soljanka am Vorabend zu. Am nächsten Tag nur noch aufwärmen.<br>
+                        Wichtig! Die Soljanka muss ein paar Stunden stehen, bevor man sie isst.<br>
+                        <br>
+                        Dazu reiche ich immer geröstetes Ciabattabrot und saure Sahne zum Einrühren.`,
+        "profilPic" : `img/profileMathiasKohler.jpg`,
+        "creator" : "Mathias Kohler",
+    },
+    ]
