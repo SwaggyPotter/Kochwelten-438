ea8042864d95fa57f78328d366566c2f1492ab63
Zwogli <Mathiaskohler@web.de>
2023-02-04 09:26:07 +0100

    add newRecipe

--- json.js
+++ json.js
@@ -27,26 +27,24 @@ let recipes = [
         // creator
         "profilPic" : `img/profileMathiasKohler.jpg`,
         "creator" : "Mathias Kohler",
+        "lablePreperationClock" : "1 Stunde 15 Minuten",
+        "lableLengthOfTime" : "1 Stunde 15 Minuten",
     },
     {
-        "name" : "Soljanka nach Mama Art",
-        "img" : `img/soljanka.jpg`,
-        "time" : "1 Stunde 15 Minuten",
-        "difficulty" : "normal",
-        "calender" : "28.01.2023",
+        "name" : "Hähnchenschenkel auf Reis",
+        "img" : `img/hähnchen-auf-reis.jpg`,
+        "time" : "15 Minuten",
+        "difficulty" : "simple",
+        "calender" : "04.02.2023",
         "portion" : 4,
-        "quantity" : ['50', '50', '50', '50', '1.25','1','37.5','1.25','0.25','0.25','0.25','0.75','','',''],
-        "unit" : ['g','g','g','g','Anzahl','Anzahl','g','Anzahl','EL','Anzahl','TL','Zehen','',''],
-        "unit" : ['g','g','g','g','Anzahl','Anzahl','g','Anzahl','EL','Anzahl','TL','Zehen','','',''],
-        "ingredient" : ['Salami','Jagdwurst','Geflügelwurst','Schinkenspeck, gewürfelt','Zwiebel(n)',`Paprikaschote(n)`,'Tomatenmark','Gewürzgurke(n)','Sambal Olek','Lorbeerblatt','Senf','Knoblauch','Gurkensud','Brühe, fette','saure Sahne'],
-        "preparation" : `Den Speck anbraten, die gewürfelte Wurst dazugeben (noch nicht die Salami!) und alles schön anbraten. Dann die Salami dazugeben. Kurz weiterbraten, die Zwiebel mit in den Topf geben und weiterbraten, bis die Zwiebeln glasig sind. Falls es ansetzen sollte, ein wenig Wasser dazugeben.<br>
-                        Nun die gewürfelten Paprika, das Tomatenmark und den zerdrückten Knoblauch dazugeben, etwas anbraten. Dann die klein geschnittenen Gurken, etwas Gurkenwasser und sämtliche Gewürze dazugeben. Mit 1 bis 1 1/2 Liter Wasser aufgießen, die fette Brühe dazugeben und 20 Minuten leicht köcheln lassen.<br>
-                        <br>
-                        Idealerweise bereitet man die Soljanka am Vorabend zu. Am nächsten Tag nur noch aufwärmen.<br>
-                        Wichtig! Die Soljanka muss ein paar Stunden stehen, bevor man sie isst.<br>
-                        <br>
-                        Dazu reiche ich immer geröstetes Ciabattabrot und saure Sahne zum Einrühren.`,
+        "quantity" : ['62.5', '0.25', '0.25', '0.25', '1','','','0.25'],
+        "unit" : ['g','Tüte /n','Liter','Becher','Anzahl','','','EL'],
+        "ingredient" : ['Reis','Gemüse, gemischt, TK','Brühe','Frischkäse','Hähnchenschenkel','Salz und Pfeffer','Paprikapulver, rosenscharf','Öl'],
+        "preparation" : `Den Frischkäse in der heißen der Brühe schmelzen. Den Reis in eine große Auflaufform verteilen, salzen und pfeffern, dann das Gemüse darüber verteilen. Die Brühe darüber gießen.
+                        Die Hähnchenkeulen mit Öl, Salz, Pfeffer, Paprika würzen und auf das Gemüse legen. Im auf 200°C vorgeheizten Ofen ca 60-80 Minuten garen. Eine genaue Zeitangabe kann ich nicht machen, da es immer variiert, der Reis muss gar sein und sollten die Hähnchenschenkel vor dem Reis fertig sein, einfach herausnehmen und warm stellen.`,
         "profilPic" : `img/profileMathiasKohler.jpg`,
         "creator" : "Mathias Kohler",
+        "lablePreperationClock" : "15 Minuten",
+        "lableLengthOfTime" : "1 Stunde 25 Minuten",
     },
     ]
